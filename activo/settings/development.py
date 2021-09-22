beb2b8445a1baa83f8de76247e75b42fa996d20f
Pacifique Clement Ndayisenga <pacifiqueclement@gmail.com>
2021-09-22 15:23:39 +0200

    Configure PostgreSQL database

--- activo/settings/development.py
+++ activo/settings/development.py
@@ -10,7 +10,11 @@ ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
     }
 }
